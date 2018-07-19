bf3f7cfef89e3d107c2bf3fbad6b6fd4f20899c8
Jose Alamo <alamose@hotmail.com>
2018-07-19 03:51:15 +0200

    Implementación preliminar de controller

--- FileServer.Facade.WebApi/Controllers/AlumnoController.cs
+++ FileServer.Facade.WebApi/Controllers/AlumnoController.cs
@@ -1,39 +1,78 @@
-﻿using System;
+﻿using FileServer.Application.Services.Contract;
+using FileServer.Application.Services.Service;
+using FileServer.Common.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace FileServer.Facade.WebApi.Controllers
 {
     public class AlumnoController : ApiController
     {
-        // GET: api/Alumno
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+		public readonly IServiceOperations<Alumno> iService = new AlumnoService();
+		public AlumnoController() : this(new AlumnoService())
+		{
 
-        // GET: api/Alumno/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+		}
+		public AlumnoController(AlumnoService alumnoService)
+		{
+			this.iService = alumnoService;
+		}
+		// GET: api/Alumno
+		public IQueryable<Alumno> Get()
+		{
+			throw new NotImplementedException();
+			//return alumnoService.GetAll();
+		}
 
-        // POST: api/Alumno
-        public void Post([FromBody]string value)
-        {
-        }
+		// GET: api/Alumno/5
+		public IHttpActionResult Get(int id)
+		{
+			throw new NotImplementedException();
+			//return alumnoService.GetByID(id);
+			//Rerotna ok alumno
+		}
 
-        // PUT: api/Alumno/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+		// POST: api/Alumno
+		[ResponseType(typeof(Alumno))]
+		public IHttpActionResult Post(Alumno alumno)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+			try
+			{
+				Alumno AlumnoInserted =
+						iService.Add(alumno);
+			}
+			catch (Exception)
+			{
+				throw;
+			}
 
-        // DELETE: api/Alumno/5
-        public void Delete(int id)
-        {
-        }
-    }
+			return CreatedAtRoute("DefaultApi",
+				new { id = alumno.Id }, alumno);
+		}
+
+		// PUT: api/Alumno/5
+		public bool Put(Alumno alumno)
+		{
+			throw new NotImplementedException();
+			//alumnoService.Update(alumno);
+			//return true;
+			//Devuelce status code no content
+		}
+
+		// DELETE: api/Alumno/5
+		public IHttpActionResult Delete(int id)
+		{
+			throw new NotImplementedException();
+			//return alumnoService.Remove(id);
+		}
+	}
 }
